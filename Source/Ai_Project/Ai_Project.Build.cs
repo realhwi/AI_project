8e8834d0a06bf398c2d29cef97504ca28ab469d3
realhwi <wlsgnl49@gmail.com>
2024-04-03 19:31:46 +0900

    test

--- Source/Ai_Project/Ai_Project.Build.cs
+++ Source/Ai_Project/Ai_Project.Build.cs
@@ -8,7 +8,7 @@ public class Ai_Project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "WebSockets", "Json" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
