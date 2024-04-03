3e792d90f541d71f4e4ef07ab409aeb1073cf20f
realhwi <wlsgnl49@gmail.com>
2024-04-03 17:13:10 +0900

    ddd

---
+++ Source/Ai_Project/Ai_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ai_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ai_Project, "Ai_Project" );
