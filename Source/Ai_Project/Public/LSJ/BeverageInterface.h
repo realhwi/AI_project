39b0c6654edcc48dfbe827a2a67ec9698397cacc
tjdwns4494 <76541391+lim-make-well@users.noreply.github.com>
2024-04-08 18:46:50 +0900

    create actors

---
+++ Source/Ai_Project/Public/LSJ/BeverageInterface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "BeverageInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UBeverageInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AI_PROJECT_API IBeverageInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
